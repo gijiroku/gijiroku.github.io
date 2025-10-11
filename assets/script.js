e3a48c0fd84d61fa08b55bd66f591d8ffe570e6f
gijiroku <tamani.log.in@gmail.com>
2025-10-11 22:40:28 +0900

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -92,6 +92,8 @@ function renderNav(){
     local=true;
   }
   path=url.split(domain)[1];
+  console.log(url);
+  console.log(domain);
   console.log(path);
   let subDir=path.split('/')[1];
   let navContent=reportNav;
